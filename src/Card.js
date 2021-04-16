d8a1c8c6e4c18730f484e8a0f52266dbbbed6ef1
Chandni <chandni1141996@gmail.com>
2021-04-16 20:47:35 +0530

    display card

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Card extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cardImg : ''
+        }
+    }
+
+    async componentDidMount() {
+        let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.props.id}/draw`);
+        this.setState({ cardImg : res.data.cards[0].image})
+
+    }
 
     render() {
+        
         return (
-            <div>Hi from card</div>
+            <div>
+                <img src = {this.state.cardImg} />
+            </div>
         )
     }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import Card from './Card';
+import axios from 'axios';
 
 class Deck extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            deckID : ''
+        }
+    }
+
+    async componentDidMount() {
+        let response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle');
+        console.log(response.data);
+
+        this.setState({deckID : response.data.deck_id});
+    }
+
     render() {
         return(
-            <Card />
+            <div>
+                <button>Gimme a card!</button>
+                {this.state.deckID && <Card id = {this.state.deckID} />}
+            </div>
+
         )
     }
 }
